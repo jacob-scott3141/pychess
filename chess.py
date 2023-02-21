3f7b199ab7f1f933908a19cff9bdb72f5491859c
jelli <jacob.scott3141@gmail.com>
2023-02-21 23:40:27 +0000

    added turn order

--- chess.py
+++ chess.py
@@ -5,6 +5,7 @@ pygame.init()
 
 # Define the size of the chess board
 board_size = 700
+board_size = board_size // 8 * 8
 
 # Create the Pygame display
 screen = pygame.display.set_mode((board_size, board_size))
@@ -79,100 +80,7 @@ square_size = board_size / 8
 font = pygame.font.SysFont("Arial", 20)
 
 # Draw the chess board and pieces
-for i in range(8):
-    for j in range(8):
-        # Calculate the position of the square
-        x = j * square_size
-        y = i * square_size
-
-        # Determine the color of the square
-        if (i + j) % 2 == 0:
-            color = white
-        else:
-            color = black
-
-        # Draw the square
-        pygame.draw.rect(screen, color, [x, y, square_size, square_size])
-
-        # Draw the piece, if there is one
-        piece_name = pieces[i * 8 + j]
-        if piece_name != "":
-            piece_image = piece_images[piece_name]
-            piece_rect = piece_image.get_rect()
-            piece_rect.center = (x + square_size / 2, y + square_size / 2)
-            screen.blit(piece_image, piece_rect)
-
-        # Draw the coordinates
-        if j == 0:
-            label = font.render(str(8 - i), True, black)
-            screen.blit(label, (x - 20, y + square_size / 2 - 10))
-        if i == 7:
-            label = font.render(chr(j + 97), True, black)
-            screen.blit(label, (x + square_size / 2 - 10, y + square_size + 10))
-
-# Update the display
-pygame.display.flip()
-
-# Define variables for tracking the current piece and its position
-current_piece = None
-current_piece_position = None
-
-# Run the Pygame event loop
-while True:
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            pygame.quit()
-            quit()
-
-        # Handle mouse button events
-        if event.type == pygame.MOUSEBUTTONDOWN:
-            # Get the mouse position in terms of the chess board
-            mouse_x, mouse_y = pygame.mouse.get_pos()
-            board_x = int(mouse_x // square_size)
-            board_y = int(mouse_y // square_size)
-
-            # Get the name and position of the piece at the mouse position
-            piece_name = pieces[board_y * 8 + board_x]
-            piece_position = (board_x, board_y)
-
-            # If there is a piece at the mouse position, pick it up
-            if piece_name != "":
-                current_piece = piece_name
-                current_piece_position = piece_position
-
-        elif event.type == pygame.MOUSEBUTTONUP:
-            # If there is a current piece, try to move it to the new position
-            if current_piece is not None:
-                # Get the mouse position in terms of the chess board
-                mouse_x, mouse_y = pygame.mouse.get_pos()
-                board_x = int(mouse_x // square_size)
-                board_y = int(mouse_y // square_size)
-                log("picked up piece")
-
-                # Get the name and position of the piece at the new position
-                new_piece_name = pieces[board_y * 8 + board_x]
-                new_piece_position = (board_x, board_y)
-
-                # If there is no piece at the new position, move the current piece there
-                if new_piece_name == "":
-                    pieces[board_y * 8 + board_x] = current_piece
-                    pieces[current_piece_position[1] * 8 + current_piece_position[0]] = ""
-                    log("moved piece")
-                # Otherwise, if there is an enemy piece at the new position, capture it and move the current piece there
-                elif current_piece[0] != new_piece_name[0]:
-                    pieces[board_y * 8 + board_x] = current_piece
-                    pieces[current_piece_position[1] * 8 + current_piece_position[0]] = ""
-                    log("taken piece")
-                # Otherwise, move the current piece back to its original position
-                else:
-                    pieces[current_piece_position[1] * 8 + current_piece_position[0]] = current_piece
-
-                # Clear the current piece
-                current_piece = None
-                current_piece_position = None
-                log(print_board())
-
-    # Draw the chess board and pieces
+def draw():
     for i in range(8):
         for j in range(8):
             # Calculate the position of the square
@@ -203,6 +111,78 @@ while True:
             if i == 7:
                 label = font.render(chr(j + 97), True, black)
                 screen.blit(label, (x + square_size / 2 - 10, y + square_size + 10))
+
+# Update the display
+pygame.display.flip()
+
+# Define variables for tracking the current piece and its position
+current_piece = None
+current_piece_position = None
+
+# Define variables for tracking player turn
+isWhite = True
+
+# Run the Pygame event loop
+while True:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            quit()
+
+        # Handle mouse button events
+        if event.type == pygame.MOUSEBUTTONDOWN:
+            # Get the mouse position in terms of the chess board
+            mouse_x, mouse_y = pygame.mouse.get_pos()
+            board_x = int(mouse_x // square_size)
+            board_y = int(mouse_y // square_size)
+
+            # Get the name and position of the piece at the mouse position
+            piece_name = pieces[board_y * 8 + board_x]
+            piece_position = (board_x, board_y)
+
+            # Can move?
+            canMove = (piece_name[0]=="w" and isWhite ) or (piece_name[0]=="b" and not isWhite )
+
+            # If there is a piece at the mouse position, pick it up
+            if piece_name != "" and canMove:
+                current_piece = piece_name
+                current_piece_position = piece_position
+
+        elif event.type == pygame.MOUSEBUTTONUP:
+            # If there is a current piece, try to move it to the new position
+            if current_piece is not None:
+                # Get the mouse position in terms of the chess board
+                mouse_x, mouse_y = pygame.mouse.get_pos()
+                board_x = int(mouse_x // square_size)
+                board_y = int(mouse_y // square_size)
+                log("picked up piece")
+
+                # Get the name and position of the piece at the new position
+                new_piece_name = pieces[board_y * 8 + board_x]
+                new_piece_position = (board_x, board_y)
+
+                # If there is no piece at the new position, move the current piece there
+                if new_piece_name == "":
+                    pieces[board_y * 8 + board_x] = current_piece
+                    pieces[current_piece_position[1] * 8 + current_piece_position[0]] = ""
+                    log("moved piece")
+                    isWhite = not isWhite
+                # Otherwise, if there is an enemy piece at the new position, capture it and move the current piece there
+                elif current_piece[0] != new_piece_name[0]:
+                    pieces[board_y * 8 + board_x] = current_piece
+                    pieces[current_piece_position[1] * 8 + current_piece_position[0]] = ""
+                    log("taken piece")
+                    isWhite = not isWhite
+                # Otherwise, move the current piece back to its original position
+                else:
+                    pieces[current_piece_position[1] * 8 + current_piece_position[0]] = current_piece
+
+                # Clear the current piece
+                current_piece = None
+                current_piece_position = None
+                log(print_board())
+
+    draw()
                 
     pygame.display.flip()
     
